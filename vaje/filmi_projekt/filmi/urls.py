5d9f7742a7bbd286304feb976423fee69e3c2d52
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2026-03-31 10:19:53 +0200

    Dodana prijava in odjava

--- vaje/filmi_projekt/filmi/urls.py
+++ vaje/filmi_projekt/filmi/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('filmi/', include('filmiapp.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
